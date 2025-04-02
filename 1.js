9e2750d6a558ecedc252c0d92f9a643ad5d3319b
Jerome Giray <jeromegiray500@gmail.com>
2025-04-02 09:33:41 +0800

    Completed No. 1

---
+++ 1.js
@@ -0,0 +1,11 @@
+//Collect user information using prompt() 
+let fullName = prompt("Deejay Amor C. Soriano:"); 
+let age = parseInt(prompt("18:")); 
+let favNumber = parseInt(prompt("23:")); 
+let favColor = prompt("Blue:");
+
+console.log("User Profile:"); 
+console.log("Full Name:"); \
+console.log("Age:"); 
+console.log("Favorite Number:");
+console.log("Favorite Color:"); 
