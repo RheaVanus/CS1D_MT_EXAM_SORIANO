4efd12478fcd063d6834395446d1823986039e48
Jerome Giray <jeromegiray500@gmail.com>
2025-04-02 09:35:50 +0800

    Completed No 2

---
+++ 2.js
@@ -0,0 +1,16 @@
+// Create a student object to store information.
+const student = {
+    subjectTitle: "Data Structures and Algorithms",
+    classSchedule: "9am Monday to Friday",
+    classroom: "Comlab B",
+    instructor: "Owen Pascua",
+    studentName: "Deejay Amor C. Soriano"
+};
+
+// Log the student information to the console.
+console.log("Student Information:");
+console.log("Subject Title:", student.subjectTitle);    
+console.log("Class Schedule:", student.classSchedule);
+console.log("Classroom:", student.classroom);
+console.log("Instructor:", student.instructor);
+console.log("Student Name:", student.studentName);
